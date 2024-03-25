2412200bdfbe72170809de78f0ed57d300fb3d07
Muhammad Momin <160387789+MuhammadMomin095@users.noreply.github.com>
2024-03-26 00:34:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+function addNumberAndString(number1, numberstring) {
+    return number1 + Number(numberstring);
+}
+console.log(addNumberAndString(10, "5"));
---
+++ app.ts
@@ -0,0 +1,5 @@
+function addNumberAndString(number1:number, numberstring:string): number {
+    return number1 + Number(numberstring);
+}
+
+console.log(addNumberAndString(10, "5"));
